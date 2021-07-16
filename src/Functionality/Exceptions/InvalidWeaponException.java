c4178d7718465737639b39e6972a00c759e12f53
Tyson McLeod <tyson.mcleod@se.experis.com>
2021-07-16 08:57:52 +0200

    minor fixes

--- src/Functionality/Exceptions/InvalidWeaponException.java
+++ src/Functionality/Exceptions/InvalidWeaponException.java
@@ -1,5 +1,5 @@
 package Functionality.Exceptions;
-
+// Invalid weapon exception for wrong char type or level
 public class InvalidWeaponException extends Exception{
     public InvalidWeaponException(String s){
         super(s);
